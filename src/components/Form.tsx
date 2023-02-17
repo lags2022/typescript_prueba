554aeae9e9e6ee29a948ba91137b4b4460dec3b7
lags2022 <lguzman.58erb@outlook.com>
2023-02-17 09:06:03 -0500

    update2

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,53 +1,14 @@
-import { useReducer } from "react";
+import useNewSubForm from "../hooks/useNewSubForm";
 import { Sub } from "../types";
-
-interface FormState {
-  inputValues: Sub;
-}
 
 interface FormProps {
   onNewSub: (newSub: Sub) => void;
 }
 
-const INITIAL_STATE = {
-  nick: "",
-  subMonths: 0,
-  avatar: "",
-  description: "",
-};
-
-type FormReducerAction =
-  | {
-      type: "change_value";
-      payload: {
-        inputName: string;
-        inputValue: string;
-      };
-    }
-  | { type: "clear" };
-
-const formReducer = (
-  state: FormState["inputValues"],
-  action: FormReducerAction
-) => {
-  switch (action.type) {
-    case "change_value":
-      const { inputName, inputValue } = action.payload;
-      return {
-        ...state,
-        [inputName]: inputValue,
-      };
-    case "clear":
-      return INITIAL_STATE;
-    default:
-      return state;
-  }
-};
-
 const Form = ({ onNewSub }: FormProps) => {
   // const [inputValues, setInputValues] = useState<FormState["inputValues"]>(INITIAL_STATE);
 
-  const [inputValues, dispatch] = useReducer(formReducer, INITIAL_STATE);
+  const [inputValues, dispatch] = useNewSubForm();
 
   const handleSubmit = (evt: React.FormEvent<HTMLFormElement>) => {
     evt.preventDefault();
