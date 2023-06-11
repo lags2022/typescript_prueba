2a0b471d253eb3facb84c64b30af7f454d40f3ad
lags2022 <lguzman.58erb@outlook.com>
2023-06-10 19:03:20 -0500

    framer motion tailwind

--- src/services/getAllSubs.ts
+++ src/services/getAllSubs.ts
@@ -2,29 +2,30 @@ import axios from "axios";
 import { Sub, SubResponseFromApi } from "../types";
 
 export const getAllSubs = async () => {
-    const apiResponse = await fetchSubs();
-    return mapFromApiToSubs(apiResponse);
-}
+  const apiResponse = await fetchSubs();
+  return mapFromApiToSubs(apiResponse);
+};
 
 const fetchSubs = async () => {
-    const response = await axios
-        .get<SubResponseFromApi>("http://localhost:3001/subs");
-    return response.data;
+  const response = await axios.get<SubResponseFromApi>(
+    "http://localhost:3001/suscribers"
+  );
+  return response.data;
 };
 
 const mapFromApiToSubs = (apiResponse: SubResponseFromApi): Array<Sub> => {
-    return apiResponse.map((subFromApi) => {
-        const {
-            nick,
-            months: subMonths,
-            profileUrl: avatar,
-            description,
-        } = subFromApi;
-        return {
-            nick,
-            subMonths,
-            avatar,
-            description,
-        };
-    });
+  return apiResponse.map((subFromApi) => {
+    const {
+      nick,
+      months: subMonths,
+      profileUrl: avatar,
+      description,
+    } = subFromApi;
+    return {
+      nick,
+      subMonths,
+      avatar,
+      description,
+    };
+  });
 };
